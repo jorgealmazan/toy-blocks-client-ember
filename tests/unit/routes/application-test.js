bcb9caf5c9d2780cb5e8b375e619ceeb13247c17
Jorge Almazan <jorge.almazan87@live.com>
2020-10-16 19:26:29 -0500

    Modified tests in order to reflect new blocks object sent to the nodes for rendering.

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -12,18 +12,92 @@ module('Unit | Route | application', function(hooks) {
 
     const expectedNodes = [
       {
+        "blocks": [
+          {
+            "data": "The Human Torch",
+            "index": "001"
+          },
+          {
+            "data": "is denied",
+            "index": "002"
+          },
+          {
+            "data": "a bank loan",
+            "index": "003"
+          }
+        ],
         "loading": false,
         "name": "Thawing Springs",
         "online": true,
         "url": "https://thawing-springs-53971.herokuapp.com"
       },
       {
+        "blocks": [
+          {
+            "data": "every",
+            "index": "001"
+          },
+          {
+            "data": "good",
+            "index": "002"
+          },
+          {
+            "data": "boy",
+            "index": "003"
+          },
+          {
+            "data": "does",
+            "index": "004"
+          },
+          {
+            "data": "fine",
+            "index": "005"
+          }
+        ],
         "loading": false,
         "name": "Secret Lowlands",
         "online": true,
         "url": "https://secret-lowlands-62331.herokuapp.com"
       },
       {
+        "blocks": [
+          {
+            "data": "By reason of these things",
+            "index": "001"
+          },
+          {
+            "data": "then the whaling voyage was welcome",
+            "index": "002"
+          },
+          {
+            "data": "the great flood-gates of the wonder-world swung open",
+            "index": "003"
+          },
+          {
+            "data": "and in the wild conceits that swayed me to my purpose",
+            "index": "004"
+          },
+          {
+            "data": "two and two there floated into my inmost soul",
+            "index": "005"
+          },
+          {
+            "data": "endless processions of the whale",
+            "index": "006"
+          },
+          {
+            "data": "and mid most of them all",
+            "index": "007"
+          },
+          {
+            "data": "one grand hooded phantom",
+            "index": "008"
+          },
+          {
+            "data": "like a snow hill in the air",
+            "index": "009"
+          }
+        ],
         "loading": false,
         "name": "Calm Anchorage",
         "online": true,
